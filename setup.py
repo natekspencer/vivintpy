ba3919ee130d53edb5f7c791daf81f5eb8cee236
Nathan Spencer <natekspencer@gmail.com>
2021-03-02 23:47:50 -0700

    Update project configuration files

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
-#!/usr/bin/env python
 """pyvivint setup script."""
+import setuptools
 
-from setuptools import find_packages, setup
-
-install_requires = ["aiohttp>=3.6", "certifi>=2019.9.11", "pubnub>=5.0.1"]
-
-setup(
-    name="pyvivint",
-    version="2021.3.0",
-    description="Python module to interact with VivintSky API.",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    author="Ovidiu Stateina, Nathan Spencer",
-    author_email="ovidiurs@gmail.com",
-    license="MIT",
-    install_requires=install_requires,
-    packages=find_packages(),
-    url="https://github.com/ovirs/pyvivint",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Home Automation",
-    ],
-)
+setuptools.setup()
