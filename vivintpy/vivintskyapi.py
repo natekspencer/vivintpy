ff7662d6443be06e6c43d8e47a59e3c26d010dea
Nathan Spencer <natekspencer@gmail.com>
2024-03-04 10:17:45 -0700

    Tweak api error handling (#144)
    * Tweak api error handling

--- vivintpy/vivintskyapi.py
+++ vivintpy/vivintskyapi.py
@@ -506,9 +506,10 @@ class VivintSkyApi:
             allow_redirects=allow_redirects,
         )
         async with resp:
-            if not resp.ok and resp.content_type != "application/json":
-                resp.raise_for_status()
-            resp_data: dict = await resp.json(encoding="utf-8")
+            if resp.content_type != "application/json":
+                resp_data = {AuthenticationResponse.MESSAGE: await resp.text()}
+            else:
+                resp_data = await resp.json(encoding="utf-8")
             if resp.status == 200:
                 return resp_data
             if resp.status == 302:
